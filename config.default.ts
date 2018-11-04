abeec9311c8a5b478fc628d2189dd7c8caa72a22
Roman Axelrod <Axel186@Gmail.com>
2018-11-04 10:48:49 +0200

    Update config.default.ts

--- config.default.ts
+++ config.default.ts
@@ -5,5 +5,5 @@ export const config = {
   projectId: 'PROJECT_ID',
   storageBucket: 'STORAGE_BUCKET',
   messagingSenderId: 'MESSAGING_SENDER_ID',
-  cloudFunctionsUrl: 'https://us-central1-game-test-c9cbd.cloudfunctions.net'
+  cloudFunctionsUrl: 'CLOUD_FUNCTIONS_URL'
 };
